69c94607eacd96cfd38ec0254265d8a34f85549c
Jeff Wang <jeffreyfranklinwang@gmail.com>
2025-02-17 17:14:35 +0000

    partial

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -5,7 +5,11 @@ import DAO.AccountDAO;
 
 public class AccountService {
     
-
+    //adds new user
+    //NEED LOGIC TO VERIFY NO DUPLICATE ACCOUNT ID
+    public Account addAccount(Account account){
+        return account;
+    }
 
 
 
