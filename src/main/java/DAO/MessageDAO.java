8ef70ea7046266e9be0d96b6fda3c79a3c5c5141
Jeff Wang <jeffreyfranklinwang@gmail.com>
2025-02-18 16:32:38 +0000

    partial

---
+++ src/main/java/DAO/MessageDAO.java
@@ -0,0 +1,70 @@
+package DAO;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Message;
+import Util.ConnectionUtil;
+
+public class MessageDAO {
+    
+    //create new message
+    public Message newMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO Message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)";
+
+            //auto-generate a message_id
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+
+            // returns message object with its message_id key
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+
+    public List<Message> getAllMessages(){
+        
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Message";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                Message message = new Message(resultSet.getInt("message_id"),
+                                resultSet.getInt("posted_by"),
+                                resultSet.getString("message_text"),
+                                resultSet.getLong("time_posted_epoch"));
+                messages.add(message);
+            } 
+
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+        return null;
+    }
+
+}
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,22 @@
+package Service;
+
+import java.util.List;
+
+import DAO.MessageDAO;
+import Model.Message;
+
+public class MessageService {
+    
+    //creates new message
+    public Message newMessage(Message message){
+        return message;
+    }
+
+    /*
+    //returns all messages
+    public List<Message> getAllMessages(){
+        return MessageDAO.getAllMessages();
+    }
+    */
+
+}
